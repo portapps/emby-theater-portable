2e4322574e5404771dd2b144fe0fcb3e5622bc62
CrazyMax <crazy-max@users.noreply.github.com>
2020-04-13 03:32:59 +0200

    Portapps 2.1.2

--- main.go
+++ main.go
@@ -6,22 +6,23 @@ import (
 	"fmt"
 	"os"
 
-	. "github.com/portapps/portapps"
-	"github.com/portapps/portapps/pkg/dialog"
-	"github.com/portapps/portapps/pkg/utl"
-	"github.com/portapps/portapps/pkg/win"
+	"github.com/portapps/portapps/v2"
+	"github.com/portapps/portapps/v2/pkg/dialog"
+	"github.com/portapps/portapps/v2/pkg/log"
+	"github.com/portapps/portapps/v2/pkg/utl"
+	"github.com/portapps/portapps/v2/pkg/win"
 )
 
 var (
-	app *App
+	app *portapps.App
 )
 
 func init() {
 	var err error
 
 	// Init app
-	if app, err = New("emby-theater-portable", "Emby Theater"); err != nil {
-		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	if app, err = portapps.New("emby-theater-portable", "Emby Theater"); err != nil {
+		log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
 	}
 }
 
@@ -31,12 +32,12 @@ func main() {
 
 	// Check arch
 	if win.Is64Arch() && utl.Exists(utl.PathJoin(app.AppPath, "x86")) {
-		Log.Error().Msg("Emby Theater win32 cannot be launched on win64 system")
+		log.Error().Msg("Emby Theater win32 cannot be launched on win64 system")
 		if _, err := dialog.MsgBox(
 			fmt.Sprintf("%s portable", app.Name),
 			"Emby Theater win32 cannot be launched on win64 system.",
 			dialog.MsgBoxBtnOk|dialog.MsgBoxIconError); err != nil {
-			Log.Error().Err(err).Msg("Cannot create dialog box")
+			log.Error().Err(err).Msg("Cannot create dialog box")
 		}
 		return
 	}
@@ -44,13 +45,13 @@ func main() {
 	// Data
 	configFile := utl.PathJoin(app.AppPath, "Emby.Theater.exe.config")
 	if err := utl.Replace(configFile, `ProgramDataPath" value=""`, `ProgramDataPath" value="../data"`); err != nil {
-		Log.Fatal().Err(err).Msg("Cannot change ProgramDataPath")
+		log.Fatal().Err(err).Msg("Cannot change ProgramDataPath")
 	}
 
 	// Cancel cec driver install
 	cecCancelFile, err := os.Create(utl.PathJoin(utl.CreateFolder(app.DataPath, "cec-driver"), "cancel"))
 	if err != nil {
-		Log.Error().Err(err).Msg("Cannot write cec-driver cancel file")
+		log.Error().Err(err).Msg("Cannot write cec-driver cancel file")
 	}
 	cecCancelFile.Close()
 
@@ -58,7 +59,7 @@ func main() {
 	systemFile := utl.PathJoin(app.DataPath, "config", "system.xml")
 	if utl.Exists(systemFile) {
 		if err := utl.Replace(systemFile, `<EnableAutoUpdate>true`, `<EnableAutoUpdate>false`); err != nil {
-			Log.Error().Err(err).Msg("Cannot disable auto update")
+			log.Error().Err(err).Msg("Cannot disable auto update")
 		}
 	}
 
